3fcea95a4f6ec8e5f81fde5c9729d57659ea3783
SongeunKim <ucss22002@gmail.com>
2026-09-16 15:51:10 +0900

    docs: pr 템플릿 추가

---
+++ src/test/java/com/better/CommuteMate/CommuteMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.better.CommuteMate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommuteMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
